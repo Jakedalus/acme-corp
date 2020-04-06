c64b33e9d01d3d1d3867c8911b8a24b90528d61d
Jakedalus <jacquedalus@gmail.com>
2020-04-06 16:09:46 -0400

    Style PostQuote component

--- src/components/postQuote.js
+++ src/components/postQuote.js
@@ -2,13 +2,22 @@ import React from 'react';
 import RichText from './richText';
 import styled from 'styled-components';
 
+const PostQuoteWrapper = styled.div`
+  background: rgba(0,0,0,0.2);
+  padding: 4px 8px;
+  font-style: italic;
+  font-weight: bold;
+  color: var(--blue);
+  border-left: 5px solid var(--blue);
+`;
+
 const PostQuote = ({quote}) => {
   console.log(quote);
 
   return (
-    <div>
+    <PostQuoteWrapper>
       <RichText render={quote} />
-    </div>
+    </PostQuoteWrapper>
   );
 };
 
